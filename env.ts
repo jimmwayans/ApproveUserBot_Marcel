2820eb365dcf7fe888c5bbee9e8f9251f2f95a15
unkn0wn_11 <jimmwayans@yahoo.com>
2024-12-04 23:21:13 +0300

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
-  OWNERS: str(),
-  MONGO_URL: str(),
+  BOT_TOKEN: "8025293689:AAFyf6smxuifoKtYeFvXhcOWiSPLBUfbJ2c",
+  OWNERS: 7951987150,
+  MONGO_URL: "mongodb+srv://expertreal1:LFx7Xa8fvmRTIPkM@cluster0.tza0w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 });
